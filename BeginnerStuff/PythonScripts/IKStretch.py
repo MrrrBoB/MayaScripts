94e24301b1970e1f4139cf7029f491039123837d
Nate <Nathoward22@gmail.com>
2023-04-17 16:11:15 -0600

    Added A separate tool for spline stretching

--- BeginnerStuff/PythonScripts/IKStretch.py
+++ BeginnerStuff/PythonScripts/IKStretch.py
@@ -7,7 +7,7 @@ def createStretchyIKWindow():
     cmds.showWindow(sIKWindow)
 
 
-sIKWindow = cmds.window(title="Make IK Stretch", widthHeight=(450, 150))
+sIKWindow = cmds.window(title="Make IK Stretch", widthHeight=(450, 175))
 cmds.columnLayout(adjustableColumn=True, rowSpacing=5)
 cmds.rowLayout(numberOfColumns=3, columnAlign=(1, 'right'))
 cmds.text(label='Start Joint')
@@ -27,11 +27,12 @@ cmds.setParent('..')
 cmds.rowLayout(numberOfColumns=2, columnAlign=(1, 'right'))
 cmds.text(label='Maximum Stretch Multiplier')
 maxStretchFloatSlider = cmds.floatSliderGrp(label="Radius Size:",
-                                        min=2,
-                                        max=10,
-                                        value=3,
-                                        field=True)
+                                            min=2,
+                                            max=10,
+                                            value=3,
+                                            field=True)
 cmds.setParent('..')
+reverseScalarCheckbox = cmds.checkBox(label='Negative Scalar Node')
 cmds.button(label='Add stretch', command='CreateStretchButtonCommand()')
 createStretchyIKWindow()
 
@@ -56,63 +57,74 @@ def CreateStretchButtonCommand():
     chosenBaseControl = cmds.textField(baseControlTextField, q=1, text=True)
     chosenIKControl = cmds.textField(IKControlTextField, q=1, text=True)
     chosenMaxStretch = cmds.floatSliderGrp(maxStretchFloatSlider, q=True, v=True)
-    CreateIKStretch(chosenStartJoint, chosenBaseControl, chosenIKControl, chosenMaxStretch)
+    needsReverseNode = cmds.checkBox(reverseScalarCheckbox, q=1, value=1)
+    CreateIKStretch(chosenStartJoint, chosenBaseControl, chosenIKControl, chosenMaxStretch, needsReverseNode)
 
 
-def CreateIKStretch(startJoint, baseControl, ikControl, maxStretchValue):
-
-# add the stretch switch and max stretch attribute to the ik Control
+def CreateIKStretch(startJoint, baseControl, ikControl, maxStretchValue, needsReverseScalar):
+    # add the stretch switch and max stretch attribute to the ik Control
     cmds.addAttr(ikControl, longName='Stretch', attributeType='float', minValue=0, maxValue=1, dv=1, keyable=True)
-    cmds.addAttr(ikControl, longName='Max_Stretch', attributeType='float', minValue=1, maxValue=maxStretchValue, dv=maxStretchValue, keyable=True)
-# add the locators
-    baseLoc = cmds.spaceLocator(n=(startJoint+'_baseStretch_Loc'))[0]
+    cmds.addAttr(ikControl, longName='Max_Stretch', attributeType='float', minValue=1, maxValue=maxStretchValue,
+                 dv=maxStretchValue, keyable=True)
+    # add the locators
+    baseLoc = cmds.spaceLocator(n=(startJoint + '_baseStretch_Loc'))[0]
     cmds.matchTransform(baseLoc, startJoint)
     cmds.parentConstraint(baseControl, baseLoc)
-    endLoc = cmds.spaceLocator(n=(startJoint+'_endStretch_Loc'))[0]
+    endLoc = cmds.spaceLocator(n=(startJoint + '_endStretch_Loc'))[0]
     cmds.matchTransform(endLoc, ikControl)
     cmds.parentConstraint(ikControl, endLoc)
-#get the list of joints under the start joint
+    # get the list of joints under the start joint
     jointHierarchy = cmds.listRelatives(startJoint, type='joint', ad=True)
     jointHierarchy.append(startJoint)
     jointHierarchy.reverse()
     totalJoints = len(jointHierarchy)
-    print('stretching %s joints' %(totalJoints))
+    print('stretching %s joints' % (totalJoints))
     placeholderName = ikControl.replace('_Ctrl', '')
-#check to make sure the end joint is the last in the heirarchy
+    # check to make sure the end joint is the last in the heirarchy
     print(jointHierarchy)
     '''print(totalJointHierarchy[len(totalJointHierarchy)-1])
     print(endJoint)
     if totalJointHierarchy[len(totalJointHierarchy)-1] is not endJoint:
         cmds.error("end joint is not the end of the joint chain")
     print(totalJointHierarchy)'''
-# add the total anatomical length of joint chain
-    totalAnatomicalLengthPMANode = cmds.createNode('plusMinusAverage', n=placeholderName+'_Stretch_Total_Anatomical_Length_PMA')
-    for i in range(totalJoints-1):
-        currentPMALengthNode = cmds.createNode('plusMinusAverage', n=jointHierarchy[i+1]+'_Length_PMA')
-        cmds.connectAttr((jointHierarchy[i+1] + '.translateX'), currentPMALengthNode+'.input1D[0]')
-        cmds.connectAttr(currentPMALengthNode+'.output1D', (totalAnatomicalLengthPMANode + '.input1D['+str(i)+']') )
-        cmds.disconnectAttr((jointHierarchy[i+1] + '.translateX'), currentPMALengthNode+'.input1D[0]')
-# create a node that holds the distance between the locators
-    distanceBetweenLocatorsNode = cmds.createNode('distanceBetween', n=placeholderName+'_Stretch_Locator_DistanceBetween')
-    cmds.connectAttr(baseLoc+'.worldMatrix', distanceBetweenLocatorsNode+'.inMatrix1')
-    cmds.connectAttr(endLoc+'.worldMatrix', distanceBetweenLocatorsNode+'.inMatrix2')
-#create switch node
-    stretchSwitchNode = cmds.createNode('multiplyDivide', n=placeholderName+'_Stretch_Switch_MD')
-    cmds.connectAttr(ikControl+'.Stretch', stretchSwitchNode+'.input1X')
-    cmds.connectAttr(distanceBetweenLocatorsNode+'.distance', stretchSwitchNode+'.input2X')
-# create scalar node
-    stretchsScalarNode = cmds.createNode('multiplyDivide', n=placeholderName+'_Stretch_Scalar_MD')
-    cmds.setAttr(stretchsScalarNode+'.operation', 2)
-    cmds.connectAttr(stretchSwitchNode+'.outputX', stretchsScalarNode+'.input1X')
-    cmds.connectAttr(totalAnatomicalLengthPMANode+'.output1D', stretchsScalarNode+'.input2X')
-# create clamp node for stretch
-    stretchClampNode = cmds.createNode('clamp', n=placeholderName+'_Stretch_Clamp')
-    cmds.setAttr(stretchClampNode+'.minR', 1)
-    cmds.connectAttr(ikControl+'.Max_Stretch', stretchClampNode+'.maxR')
-    cmds.connectAttr(stretchsScalarNode+".outputX", stretchClampNode+'.inputR')
-# connect the scalar to each joint xtranslate with an MD for each joint
+    # add the total anatomical length of joint chain
+    totalAnatomicalLengthPMANode = cmds.createNode('plusMinusAverage',
+                                                   n=placeholderName + '_Stretch_Total_Anatomical_Length_PMA')
     for i in range(totalJoints - 1):
-        currentMDNode = cmds.createNode('multiplyDivide', n=jointHierarchy[i+1]+'_Stretch_Scalar_MD')
-        cmds.connectAttr(stretchClampNode+'.outputR', currentMDNode+'.input1X')
-        cmds.connectAttr(jointHierarchy[i+1]+'_Length_PMA.output1D', currentMDNode+'.input2X')
-        cmds.connectAttr(currentMDNode+'.outputX', jointHierarchy[i+1]+'.translateX')
+        currentPMALengthNode = cmds.createNode('plusMinusAverage', n=jointHierarchy[i + 1] + '_Length_PMA')
+        cmds.connectAttr((jointHierarchy[i + 1] + '.translateX'), currentPMALengthNode + '.input1D[0]')
+        cmds.connectAttr(currentPMALengthNode + '.output1D',
+                         (totalAnatomicalLengthPMANode + '.input1D[' + str(i) + ']'))
+        cmds.disconnectAttr((jointHierarchy[i + 1] + '.translateX'), currentPMALengthNode + '.input1D[0]')
+    # create a node that holds the distance between the locators
+    distanceBetweenLocatorsNode = cmds.createNode('distanceBetween',
+                                                  n=placeholderName + '_Stretch_Locator_DistanceBetween')
+    cmds.connectAttr(baseLoc + '.worldMatrix', distanceBetweenLocatorsNode + '.inMatrix1')
+    cmds.connectAttr(endLoc + '.worldMatrix', distanceBetweenLocatorsNode + '.inMatrix2')
+    # create switch node
+    stretchSwitchNode = cmds.createNode('multiplyDivide', n=placeholderName + '_Stretch_Switch_MD')
+    cmds.connectAttr(ikControl + '.Stretch', stretchSwitchNode + '.input1X')
+    cmds.connectAttr(distanceBetweenLocatorsNode + '.distance', stretchSwitchNode + '.input2X')
+    # create scalar node
+    stretchsScalarNode = cmds.createNode('multiplyDivide', n=placeholderName + '_Stretch_Scalar_MD')
+    cmds.setAttr(stretchsScalarNode + '.operation', 2)
+    if needsReverseScalar:
+        scalarReverseNode = cmds.createNode('multiplyDivide', n=placeholderName + '_negativeScalarMD')
+        cmds.setAttr(scalarReverseNode + '.input2X', -1)
+        cmds.connectAttr(totalAnatomicalLengthPMANode + '.output1D', scalarReverseNode + '.input1X')
+        cmds.connectAttr(scalarReverseNode + '.outputX', stretchsScalarNode + '.input2X')
+    else:
+        cmds.connectAttr(totalAnatomicalLengthPMANode + '.output1D', stretchsScalarNode + '.input2X')
+    cmds.connectAttr(stretchSwitchNode + '.outputX', stretchsScalarNode + '.input1X')
+    # create clamp node for stretch
+    stretchClampNode = cmds.createNode('clamp', n=placeholderName + '_Stretch_Clamp')
+    cmds.setAttr(stretchClampNode + '.minR', 1)
+    cmds.connectAttr(ikControl + '.Max_Stretch', stretchClampNode + '.maxR')
+
+    cmds.connectAttr(stretchsScalarNode + ".outputX", stretchClampNode + '.inputR')
+    # connect the scalar to each joint xtranslate with an MD for each joint
+    for i in range(totalJoints - 1):
+        currentMDNode = cmds.createNode('multiplyDivide', n=jointHierarchy[i + 1] + '_Stretch_Scalar_MD')
+        cmds.connectAttr(stretchClampNode + '.outputR', currentMDNode + '.input1X')
+        cmds.connectAttr(jointHierarchy[i + 1] + '_Length_PMA.output1D', currentMDNode + '.input2X')
+        cmds.connectAttr(currentMDNode + '.outputX', jointHierarchy[i + 1] + '.translateX')
---
+++ BeginnerStuff/PythonScripts/SplineStretch.py
@@ -0,0 +1,107 @@
+import maya.cmds as cmds
+
+
+def createStretchySplineWindow():
+    if cmds.window("sIKWindow", exists=True):
+        cmds.deleteUI("sIKWindow")
+    cmds.showWindow(sSplineWindow)
+
+
+sSplineWindow = cmds.window(title="Make IK Stretch", widthHeight=(450, 150))
+cmds.columnLayout(adjustableColumn=True, rowSpacing=5)
+cmds.rowLayout(numberOfColumns=3, columnAlign=(1, 'right'))
+cmds.text(label='Start Joint')
+firstJointTextField = cmds.textField(placeholderText='ex:"Spline_01_Jnt"')
+cmds.button(label='getSelection', command='getStartJoint()')
+cmds.setParent('..')
+cmds.rowLayout(numberOfColumns=3, columnAlign=(1, 'right'))
+cmds.text(label='Curve')
+curveTextField = cmds.textField(placeholderText='ex:"Tail_Curve"')
+cmds.button(label='getSelection', command='getBaseControl()')
+cmds.setParent('..')
+cmds.rowLayout(numberOfColumns=3, columnAlign=(1, 'right'))
+cmds.text(label='Stretch Controller')
+splineControlTextField = cmds.textField(placeholderText='ex:"Spline_Base_Ctrl"')
+cmds.button(label='getSelection', command='getSplineControl()')
+cmds.setParent('..')
+reverseScalarCheckBox = cmds.checkBox(label='Negative Scalar Node (Use for mirrored joints)')
+cmds.button(label='Add stretch', command='CreateStretchButtonCommand()')
+createStretchySplineWindow()
+
+
+def getStartJoint():
+    selection = cmds.ls(sl=1)[0]
+    cmds.textField(firstJointTextField, e=True, text=selection)
+
+
+def getBaseControl():
+    selection = cmds.ls(sl=1)[0]
+    cmds.textField(curveTextField, e=True, text=selection)
+
+
+def getSplineControl():
+    selection = cmds.ls(sl=1)[0]
+    cmds.textField(splineControlTextField, e=True, text=selection)
+
+
+def CreateStretchButtonCommand():
+    chosenStartJoint = cmds.textField(firstJointTextField, q=1, text=1)
+    chosenCurve = cmds.textField(curveTextField, q=1, text=True)
+    chosenSplineControl = cmds.textField(splineControlTextField, q=1, text=True)
+    needsReverseNode = cmds.checkBox(reverseScalarCheckBox, q=1, value=1)
+    CreateIKStretch(chosenStartJoint, chosenCurve, chosenSplineControl, needsReverseNode)
+
+
+def CreateIKStretch(startJoint, splineCurve, splineControl, needsReverseScalar):
+
+# add the stretch switch and max stretch attribute to the spline Control
+    cmds.addAttr(splineControl, longName='Stretch', attributeType='float', minValue=0, maxValue=1, dv=1, keyable=True)
+    placeholderName = splineControl.replace('_Ctrl', '')
+# get the shape node from the curve
+    curveShapeNode = cmds.listRelatives(splineCurve, shapes=True)[0]
+    curveInfoNode = cmds.createNode('curveInfo', n=placeholderName+'_CurveInfo')
+    cmds.connectAttr(curveShapeNode+'.worldSpace', curveInfoNode+'.inputCurve')
+#create switch node
+    stretchSwitchNode = cmds.createNode('multiplyDivide', n=placeholderName+'_Stretch_Switch_MD')
+    cmds.connectAttr(splineControl + '.Stretch', stretchSwitchNode + '.input1X')
+    cmds.connectAttr(curveInfoNode+'.arcLength', stretchSwitchNode+'.input2X')
+#get the list of joints under the start joint
+    jointHierarchy = cmds.listRelatives(startJoint, type='joint', ad=True)
+    jointHierarchy.append(startJoint)
+    jointHierarchy.reverse()
+    totalJoints = len(jointHierarchy)
+    print('stretching %s joints' %(totalJoints))
+
+#check to make sure the end joint is the last in the heirarchy
+    print(jointHierarchy)
+    '''print(totalJointHierarchy[len(totalJointHierarchy)-1])
+    print(endJoint)
+    if totalJointHierarchy[len(totalJointHierarchy)-1] is not endJoint:
+        cmds.error("end joint is not the end of the joint chain")
+    print(totalJointHierarchy)'''
+# add the total anatomical length of joint chain
+    totalAnatomicalLengthPMANode = cmds.createNode('plusMinusAverage', n=placeholderName+'_Stretch_Total_Anatomical_Length_PMA')
+    for i in range(totalJoints-1):
+        currentPMALengthNode = cmds.createNode('plusMinusAverage', n=jointHierarchy[i+1]+'_Length_PMA')
+        cmds.connectAttr((jointHierarchy[i+1] + '.translateX'), currentPMALengthNode+'.input1D[0]')
+        cmds.connectAttr(currentPMALengthNode+'.output1D', (totalAnatomicalLengthPMANode + '.input1D['+str(i)+']') )
+        cmds.disconnectAttr((jointHierarchy[i+1] + '.translateX'), currentPMALengthNode+'.input1D[0]')
+
+# create scalar node
+    stretchsScalarNode = cmds.createNode('multiplyDivide', n=placeholderName+'_Stretch_Scalar_MD')
+    cmds.setAttr(stretchsScalarNode+'.operation', 2)
+    cmds.connectAttr(stretchSwitchNode+'.outputX', stretchsScalarNode+'.input1X')
+    # add a reverse scalar node if needed
+    if needsReverseScalar:
+        scalarReverseNode = cmds.createNode('multiplyDivide', n=placeholderName+'_negativeScalarMD')
+        cmds.setAttr(scalarReverseNode+'.input2X', -1)
+        cmds.connectAttr(totalAnatomicalLengthPMANode+'.output1D', scalarReverseNode+'.input1X')
+        cmds.connectAttr(scalarReverseNode+'.outputX', stretchsScalarNode+'.input2X')
+    else:
+        cmds.connectAttr(totalAnatomicalLengthPMANode+'.output1D', stretchsScalarNode+'.input2X')
+# connect the scalar to each joint xtranslate with an MD for each joint
+    for i in range(totalJoints - 1):
+        currentMDNode = cmds.createNode('multiplyDivide', n=jointHierarchy[i+1]+'_Stretch_Scalar_MD')
+        cmds.connectAttr(stretchsScalarNode+'.outputX', currentMDNode+'.input1X')
+        cmds.connectAttr(jointHierarchy[i+1]+'_Length_PMA.output1D', currentMDNode+'.input2X')
+        cmds.connectAttr(currentMDNode+'.outputX', jointHierarchy[i+1]+'.translateX')
