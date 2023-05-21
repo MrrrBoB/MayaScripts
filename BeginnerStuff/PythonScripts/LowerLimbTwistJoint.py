74d77b29f3f184c9a3d291d8db2409c6802a35d1
Nate <Nathoward22@gmail.com>
2023-05-20 19:13:00 -0600

    added options to tool for offset Axis and offset value

--- BeginnerStuff/PythonScripts/LowerLimbTwistJoint.py
+++ BeginnerStuff/PythonScripts/LowerLimbTwistJoint.py
@@ -1,9 +1,9 @@
 import maya.cmds as cmds
 
 
-def createShoulderTwistJoint(placeholderName, numJoints, firstJoint, secondJoint, zDirection, endOfChain):
+def createShoulderTwistJoint(placeholderName, numJoints, firstJoint, secondJoint, sAxis, locatorOffset, zDirection,
+                             endOfChain):
     isShoulder = True
-
 
     aimLoc = cmds.spaceLocator(n=placeholderName + '_Aim_Loc')[0]
     locGrp = cmds.group(aimLoc, n=placeholderName + '_Twist_Loc_Grp')
@@ -11,18 +11,18 @@ def createShoulderTwistJoint(placeholderName, numJoints, firstJoint, secondJoint
     targetLoc = cmds.duplicate(aimLoc, n=placeholderName + '_Target_Loc')[0]
     cmds.matchTransform(targetLoc, secondJoint, pos=1)
     upTargetLoc = cmds.duplicate(targetLoc, n=placeholderName + '_Up_Target')[0]
-    cmds.xform(upTargetLoc, r=1, t=(0, -3 * zDirection, 0))
+    cmds.xform(upTargetLoc, r=1, t=getLocatorAxis(locatorOffset, sAxis, zDirection))
     cmds.parentConstraint(firstJoint, locGrp, mo=1)
     cmds.scaleConstraint(firstJoint, locGrp)
     cmds.pointConstraint(secondJoint, targetLoc, mo=0)
-    cmds.aimConstraint(targetLoc, aimLoc, o=(0, 0, 0), aim=(zDirection, 0, 0), u=(0, -1 * zDirection, 0), wut='object',
+    cmds.aimConstraint(targetLoc, aimLoc, o=(0, 0, 0), aim=(zDirection, 0, 0), u=getUpAxis(sAxis, zDirection), wut='object',
                        wuo=upTargetLoc)
     # create the joints
     ikJointOne = cmds.joint(n=placeholderName + '_IK_Joint_1')
     ikJointTwo = cmds.duplicate(ikJointOne, n=placeholderName + '_IK_Joint_2')[0]
     cmds.parent(ikJointTwo, ikJointOne)
     cmds.matchTransform(ikJointOne, firstJoint)
-    cmds.xform(ikJointOne, r=1, os=1, t=(0, -3 * zDirection, 0))
+    cmds.xform(ikJointOne, r=1, os=1, t=getLocatorAxis(locatorOffset, sAxis, zDirection))
     cmds.matchTransform(ikJointTwo, secondJoint, pos=1)
     cmds.joint(ikJointOne, e=1, oj='xyz', zso=1)
     cmds.joint(ikJointTwo, e=1, oj='none', zso=1)
@@ -33,19 +33,19 @@ def createShoulderTwistJoint(placeholderName, numJoints, firstJoint, secondJoint
     ikJointGrp = cmds.group(w=1, em=1, n=placeholderName + '_IK_Joint_Grp')
     cmds.matchTransform(ikJointGrp, ikJointOne)
     cmds.parent(ikJointOne, ikJointGrp)
-    cmds.setAttr(ikJointGrp + '.rotateX', cmds.getAttr(ikJointGrp + '.rotateX') + (90*zDirection))
+    cmds.setAttr(ikJointGrp + '.rotateX', cmds.getAttr(ikJointGrp + '.rotateX') + (90 * zDirection))
     # create and handle the IK Handle
     ikHandle = placeholderName + '_IK_Jnt_Handle'
     cmds.ikHandle(n=ikHandle, sol='ikRPsolver', sj=ikJointOne, ee=ikJointTwo)
     cmds.pointConstraint(secondJoint, ikHandle, mo=0)
     ikJntPVLoc = cmds.spaceLocator(n=placeholderName + '_IK_Handle_PV_Loc')
     cmds.matchTransform(ikJntPVLoc, ikJointOne)
-    cmds.xform(ikJntPVLoc, os=1, r=1, t=(0, -2, 0))
+    cmds.xform(ikJntPVLoc, os=1, r=1, t=(0, -locatorOffset, 0))
     cmds.poleVectorConstraint(ikJntPVLoc, ikHandle)
     # connect the systems
     cmds.parentConstraint(ikJointOne, upTargetLoc, mo=1)
-    jointSystemGrp = cmds.group(ikJntPVLoc, ikJointGrp, ikHandle, n=placeholderName+'_IK_Jnt_System')
-    metaGrp = cmds.group(locGrp, jointSystemGrp, n=placeholderName+'_Master_Grp')
+    jointSystemGrp = cmds.group(ikJntPVLoc, ikJointGrp, ikHandle, n=placeholderName + '_IK_Jnt_System')
+    metaGrp = cmds.group(locGrp, jointSystemGrp, n=placeholderName + '_Master_Grp')
     createMidTwists(placeholderName, numJoints, firstJoint, secondJoint, zDirection, endOfChain, isShoulder, aimLoc,
                     targetLoc)
     parentJointCheck = cmds.listRelatives(firstJoint, p=True)
@@ -99,7 +99,7 @@ def createMidTwists(placeholderName, numJoints, firstJoint, secondJoint, zDirect
         cmds.setAttr(endTwistJoint + '.radius', smallRadius)
 
 
-def createTwistJoint(placeholderName, numJoints, firstJoint, secondJoint, zDirection, endOfChain):
+def createTwistJoint(placeholderName, numJoints, firstJoint, secondJoint, sAxis, locatorOffset, zDirection, endOfChain):
     isShoulder = False
     # create the aim loc
     aimLoc = cmds.spaceLocator(n=placeholderName + '_Aim_Loc')[0]
@@ -114,16 +114,33 @@ def createTwistJoint(placeholderName, numJoints, firstJoint, secondJoint, zDirec
     cmds.matchTransform(targetLoc, firstJoint)
     # create the up locator
     upTargetLoc = cmds.duplicate(aimLoc, n=placeholderName + '_Up_Target')[0]
-    cmds.xform(upTargetLoc, r=1, t=(0, 0, -3 * zDirection))
+    cmds.xform(upTargetLoc, r=1, t=getLocatorAxis(locatorOffset, sAxis, zDirection))
     cmds.parentConstraint(secondJoint, upTargetLoc, mo=1)
     cmds.pointConstraint(secondJoint, aimLoc)
     # create the aim constraint
     print(upTargetLoc)
     # if addConstraint:
-    cmds.aimConstraint(targetLoc, aimLoc, o=(0, 0, 0), aim=(-1, 0, 0), u=(0, 0, -zDirection), wut='object',
+    cmds.aimConstraint(targetLoc, aimLoc, o=(0, 0, 0), aim=(-zDirection, 0, 0), u=getUpAxis(sAxis, zDirection), wut='object',
                        wuo=upTargetLoc)
     createMidTwists(placeholderName, numJoints, firstJoint, secondJoint, zDirection, endOfChain, isShoulder, aimLoc,
                     targetLoc)
+
+def getUpAxis(axis, zDirection):
+    if axis == 1:
+        return (-zDirection, 0, 0)
+    elif axis == 2:
+        return (0, -zDirection, 0)
+    elif axis == 3:
+        return (0, 0, -zDirection)
+
+
+def getLocatorAxis(locatorOffset, axis, zDirection):
+    if axis == 1:
+        return (-locatorOffset * zDirection, 0, 0)
+    elif axis == 2:
+        return (0, -locatorOffset * zDirection, 0)
+    elif axis == 3:
+        return (0, 0, -locatorOffset * zDirection)
 
 
 def createLowerTwistJointWindow():
@@ -132,7 +149,7 @@ def createLowerTwistJointWindow():
     cmds.showWindow(ltWindow)
 
 
-ltWindow = cmds.window(title="Add Roll Joint", widthHeight=(400, 160))
+ltWindow = cmds.window(title="Add Roll Joint", widthHeight=(400, 225))
 cmds.columnLayout(adjustableColumn=True, rowSpacing=5)
 cmds.rowLayout(numberOfColumns=2, columnAlign=(1, 'right'))
 cmds.text(label='System Name')
@@ -141,6 +158,9 @@ cmds.setParent('..')
 cmds.rowLayout(numberOfColumns=2, columnAlign=(1, 'right'))
 cmds.text(label='Number of Twist Joints')
 numJField = cmds.intField(min=1, v=1)
+cmds.setParent('..')
+cmds.rowLayout(numberOfColumns=1, columnAlign=(1, 'right'))
+axisSelection = cmds.radioButtonGrp(label='Locator Axis', labelArray3=['x', 'y', 'z'], numberOfRadioButtons=3, select=1)
 cmds.setParent('..')
 cmds.rowLayout(numberOfColumns=3, columnAlign=(1, 'right'))
 cmds.text(label='Start Joint')
@@ -156,6 +176,10 @@ cmds.rowLayout(numberOfColumns=3, columnAlign=(1, 'right'))
 shoulderBox = cmds.checkBox(label='Shoulder')
 endOfChainBox = cmds.checkBox(label='End Of Chain')
 mirroredJointBox = cmds.checkBox(label='Mirrored Joint')
+cmds.setParent('..')
+cmds.rowLayout(numberOfColumns=2, columnAlign=(1, 'right'))
+cmds.text(label='Locator Offset')
+locatorOffsetFloatField = cmds.floatField(min=.01, value=1)
 cmds.setParent('..')
 cmds.button(label='create', command='twistButtonCommand()')
 createLowerTwistJointWindow()
@@ -181,9 +205,11 @@ def twistButtonCommand():
         zDir = 1
     nameF = cmds.textField(nameField, q=1, text=1)
     nameF = nameF.replace(' ', '_')
+    offsetDistance = cmds.floatField(locatorOffsetFloatField, q=1, value=1)
     endChain = cmds.checkBox(endOfChainBox, q=1, v=1)
+    sAxis = cmds.radioButtonGrp(axisSelection, q=True, select=True)
     if cmds.checkBox(shoulderBox, q=1, v=1):
-        createShoulderTwistJoint(nameF, numJ, chosenFirst, chosenSecond, zDir, endChain)
+        createShoulderTwistJoint(nameF, numJ, chosenFirst, chosenSecond, sAxis, offsetDistance, zDir, endChain)
     else:
-        createTwistJoint(nameF, numJ, chosenFirst, chosenSecond, zDir, endChain)
+        createTwistJoint(nameF, numJ, chosenFirst, chosenSecond, sAxis, offsetDistance, zDir, endChain)
     # negative z is mirrored
