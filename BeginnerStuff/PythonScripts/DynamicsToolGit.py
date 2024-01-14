67918ff75dc781a2f0afa96aa547cd38f1857292
MrrrBoB <10805477@my.uvu.edu>
2024-01-13 19:55:10 -0700

    Dynamic tool now makes curves from joint chains

--- BeginnerStuff/PythonScripts/DynamicsToolGit.py
+++ BeginnerStuff/PythonScripts/DynamicsToolGit.py
@@ -5,6 +5,26 @@ def MakeExistingCurveDynamic(existingCurve, attributeHandle):
     cmds.duplicate(existingCurve, n=existingCurve + " dynamicDuplicate")
 
 
+def CreateSplineFromJoints(firstJoint):
+    jointList = cmds.listRelatives(firstJoint, ad=True)
+    jointList.append(firstJoint)
+    jointList.reverse()
+    jointPositions = [cmds.xform(joint, q=1, t=1, ws=1)for joint in jointList]
+    newCurve = cmds.curve(n=firstJoint[0]+"_Curve", ep=jointPositions, d=3)
+    splineHandle = cmds.ikHandle(n=firstJoint[0]+"_Spline_IK_Handle",
+                                 sol='ikSplineSolver',
+                                 sj=firstJoint[0],
+                                 ee=jointList[len(jointList)-1],
+                                 c=newCurve,
+                                 ccv=0,
+                                 roc=0,
+                                 pcv=0)
+    return newCurve
+
+
+
+
+
 def createDynamicsWindow():
     if cmds.window("dynamicsWindow", exists=True):
         cmds.deleteUI("dynamicsWindow")
@@ -14,7 +34,8 @@ def createDynamicsWindow():
 dynamicsWindow = cmds.window(title="Dynamic Curves", widthHeight=(400, 225))
 cmds.columnLayout(adjustableColumn=True, rowSpacing=5)
 cmds.rowLayout(numberOfColumns=1, columnAlign=(1, 'right'))
-useExistingInputBox = cmds.checkBox(label='Use existing spline curve', v=1, ofc='existingCheckboxOn(0)', onc='existingCheckboxOn(1)')
+useExistingInputBox = cmds.checkBox(label='Use existing spline curve', v=1, ofc='existingCheckboxOn(0)',
+                                    onc='existingCheckboxOn(1)')
 cmds.setParent('..')
 existingCurveRow = cmds.rowLayout(numberOfColumns=3, columnAlign=(1, 'right'))
 cmds.text(label='Curve')
@@ -32,7 +53,6 @@ cmds.textField(placeholderText='eg. spline_IK_Handle')
 cmds.button(label='Get Selection', command='getControl')
 cmds.setParent('..')
 cmds.button(label='create', command='makeDynamicButtonCmd')
-
 
 createDynamicsWindow()
 
@@ -56,5 +76,3 @@ def makeDynamicButtonCmd():
 def existingCheckboxOn(value):
     cmds.rowLayout(existingCurveRow, e=1, vis=value)
     cmds.rowLayout(nonExistingCurveRow, e=1, vis=not value)
-
-
