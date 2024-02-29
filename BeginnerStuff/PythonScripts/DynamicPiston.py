1368ce6c741c2cbaa0206c27f2f147125d508c54
MrrrBoB <10805477@my.uvu.edu>
2024-02-29 14:36:13 -0700

    Finished iteration of piston setup

--- BeginnerStuff/PythonScripts/DynamicPiston.py
+++ BeginnerStuff/PythonScripts/DynamicPiston.py
@@ -8,8 +8,8 @@ def CreatePiston(systemName, outVector, autoConstrainUpTarget, rotatingBase):
         return
     housingLocator = sels[0]
     pistonLocator = sels[1]
-    housingLocator = cmds.rename(housingLocator, systemName+'_Housing_Loc')
-    pistonLocator = cmds.rename(pistonLocator, systemName+'_Piston_Loc')
+    housingLocator = cmds.rename(housingLocator, systemName + '_Housing_Loc')
+    pistonLocator = cmds.rename(pistonLocator, systemName + '_Piston_Loc')
     print(housingLocator + " pushes " + pistonLocator)
 
     # aim locators at eachother
@@ -28,20 +28,24 @@ def CreatePiston(systemName, outVector, autoConstrainUpTarget, rotatingBase):
         cmds.parentConstraint(pistonLocator, pistonUpLoc, mo=1)
         cmds.parentConstraint(housingLocator, housingUpLoc, mo=1)
 
-    #piston Joints
-    pistonJoint1 = cmds.joint(n=systemName+'_pistonJnt1')
+    # piston Joints
+    pistonJoint1 = cmds.joint(n=systemName + '_pistonJnt1')
     cmds.matchTransform(pistonJoint1, pistonLocator)
-    pistonJoint2 = cmds.joint(n=systemName+'_pistonJnt2')
+    pistonJoint2 = cmds.joint(n=systemName + '_pistonJnt2')
     pc = cmds.pointConstraint(pistonLocator, housingLocator, pistonJoint2, mo=0)
     cmds.delete(pc)
-    cmds.setAttr(pistonJoint1+'.rotateX', 180)
+    cmds.setAttr(pistonJoint1 + '.rotateX', 180)
     cmds.makeIdentity(pistonJoint1, r=1, a=1)
-    pistonIkHandle = cmds.ikHandle(n=systemName+'_pistonIKHandle', sj=pistonJoint1, ee=pistonJoint2, sol='ikRPsolver')[0]
+    pistonIkHandle = \
+    cmds.ikHandle(n=systemName + '_pistonIKHandle', sj=pistonJoint1, ee=pistonJoint2, sol='ikRPsolver')[0]
     cmds.select(cl=1)
     cmds.poleVectorConstraint(pistonUpLoc, pistonIkHandle)
     cmds.matchTransform(pistonIkHandle, housingLocator)
     cmds.parent(pistonIkHandle, housingLocator)
-    #housing Joints
+    cmds.pointConstraint(pistonLocator, pistonJoint1)
+
+    # housing Joints
+    cmds.select(cl=1)
     housingJoint1 = cmds.joint(n=systemName + '_housingJnt1')
     cmds.matchTransform(housingJoint1, housingLocator)
     housingJoint2 = cmds.joint(n=systemName + '_housingJnt2')
@@ -49,15 +53,20 @@ def CreatePiston(systemName, outVector, autoConstrainUpTarget, rotatingBase):
     cmds.delete(pc)
     cmds.setAttr(housingJoint1 + '.rotateX', 180)
     cmds.makeIdentity(housingJoint1, r=1, a=1)
-    housingIkHandle =cmds.ikHandle(n=systemName + '_housingIKHandle', sj=housingJoint1, ee=housingJoint2, sol='ikRPsolver')[0]
+    housingIkHandle = \
+        cmds.ikHandle(n=systemName + '_housingIKHandle', sj=housingJoint1, ee=housingJoint2, sol='ikRPsolver')[0]
     cmds.select(cl=1)
     cmds.poleVectorConstraint(housingUpLoc, housingIkHandle)
     cmds.matchTransform(housingIkHandle, pistonLocator)
     cmds.parent(housingIkHandle, pistonLocator)
-
-
-
-
+    cmds.pointConstraint(housingLocator, housingJoint1)
+    if rotatingBase:
+        cmds.select(housingJoint1)
+        baseJoint = cmds.joint(n=systemName + '_housingPivotJoint')
+        cmds.aimConstraint(housingUpLoc, baseJoint, wut='object', wuo=pistonJoint1, aim=(0, -1, 0), u=(1, 0, 0))
+        cmds.select(pistonJoint1)
+        baseJoint = cmds.joint(n=systemName + '_pistonPivotJoint')
+        cmds.aimConstraint(pistonUpLoc, baseJoint, wut='object', wuo=housingJoint1, aim=(0, -1, 0), u=(1, 0, 0))
 
 
 CreatePiston("test2", (0, 1, 0), True, True)
